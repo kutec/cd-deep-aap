29688f14ffc886bd203414d312144e36d4ebd683
kutec <ku@kutec.co.in>
2019-07-23 17:31:04 +0530

    added material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { ALL_MATERIAL } from './material.index'
+
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { LeavesComponent } from './components/leaves/leaves.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [ BrowserModule, FormsModule, BrowserAnimationsModule, ALL_MATERIAL ],
+  declarations: [ AppComponent, LeavesComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/material.index.ts
@@ -0,0 +1,9 @@
+import {
+  MatButtonModule,
+  MatCheckboxModule
+} from '@angular/material';
+
+export const ALL_MATERIAL = [
+  MatButtonModule,
+  MatCheckboxModule
+]
